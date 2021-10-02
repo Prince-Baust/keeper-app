2d0676530d5ed18f589b012d0b9910cdc56ce7ae
Md. Maruf Al Hossain Prince <pmaruf42@gmail.com>
2021-10-03 02:25:35 +0600

    createNote function moved inside map function as anonymous function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,18 +5,17 @@ import Footer from "./Footer";
 import notes from '../notes'
 
 
-const NoteCreator = (note) => {
-    return(
-      <Note
-        key = {note.key}
-        title = {note.title}
-        content = {note.content}/>)
-}
+
+
 
 const App = () =>{
     return (<div>
         <Header />
-        {notes.map(NoteCreator)}
+        {notes.map((note) =>
+            <Note
+            key = {note.key}
+            title = {note.title}
+            content = {note.content}/>)}
         <Footer />
     </div>);
 }
