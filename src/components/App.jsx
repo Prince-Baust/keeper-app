3ef7a61fcc3c9a6431be2c17db9b4c773834d6e9
Md. Maruf Al Hossain Prince <pmaruf42@gmail.com>
2021-10-03 02:17:17 +0600

    dynamically load notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,21 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import notes from '../notes'
+
+
+const NoteCreator = (note) => {
+    return(
+      <Note
+        key = {note.key}
+        title = {note.title}
+        content = {note.content}/>)
+}
 
 const App = () =>{
     return (<div>
         <Header />
-        <Note />
+        {notes.map(NoteCreator)}
         <Footer />
     </div>);
 }
